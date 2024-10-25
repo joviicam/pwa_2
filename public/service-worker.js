e3193751f1f7f8167e8bf643586ce49ef61922cd
Joviicam Racoon <joviicam9@gmail.com>
2024-10-25 17:22:39 -0600

    [ADD] update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,53 +2,86 @@ const STATIC_CACHE_NAME = "static-cache-v1.1";
 const INMUTABLE_CACHE_NAME = "inmutable-cache-v1.1";
 const DYNAMIC_CACHE_NAME = "dynamic-cache-v1.1";
 
-self.addEventListener("install", function (event) {
+/* self.addEventListener("install", function (event) {
   console.log("SW Registrado");
 
-  //DATOS DE APPSHELL
   const respCache = caches.open(STATIC_CACHE_NAME).then((cache) => {
     return cache.addAll(["/", "/index.html", "/css/style.css", "/js/app.js"]);
   });
 
-  //event.waitUntil(respCache)
-
-  //Rutas inmutables (que son CDN, o recursos que usamos)
   const respCacheInmutable = caches.open(INMUTABLE_CACHE_NAME).then((cache) => {
     return cache.addAll([
       "https://reqres.in/api/users",
       "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
       "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js",
       "https://unpkg.com/sweetalert/dist/sweetalert.min.js",
+      "http://localhost:3000/registro1.html",
+      "./images/mayuri.png",
     ]);
   });
 
-  event.waitUntil(Promise.all[(respCache, respCacheInmutable)]); //espera a que se terminen de crear esos dos caches.
+  event.waitUntil(Promise.all([respCache, respCacheInmutable]));
+}); */
 
-  //cache2.waitUntil(cache2)
+const files = [
+  "https://reqres.in/api/users",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js",
+  "https://unpkg.com/sweetalert/dist/sweetalert.min.js",
+  "http://localhost:3000/registro2.html",
+  "./images/shoto.png",
+  "./images/misamisa.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(INMUTABLE_CACHE_NAME).then((cache) => {
+      return cache.addAll(files);
+    })
+  );
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log(
-    "used to intercept requests so we can check for the file or data in the cache"
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((fetchResponse) => {
+          return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            cleanCache(DYNAMIC_CACHE_NAME, 50);
+            return fetchResponse;
+          });
+        })
+      );
+    })
   );
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("this event triggers when the service worker activates");
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter(
+              (cacheName) =>
+                cacheName !== STATIC_CACHE_NAME &&
+                cacheName !== INMUTABLE_CACHE_NAME
+            )
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
+  );
 });
 
+// Función para limpiar el caché
 const cleanCache = (cacheName, maxSize) => {
-  //recibes un máximo de caches
   caches.open(cacheName).then((cache) => {
-    return cache.keys().then((items) => {
-      console.log(items.length);
+    cache.keys().then((items) => {
       if (items.length >= maxSize) {
-        //comparar si se supera el tamaño de caches
-        cache
-          .delete(items[0]) //eliminar el primer cache
-          .then(() => {
-            cleanCache(cacheName, maxSize);
-          }); //revisar si no hay más caches para eliminar.
+        cache.delete(items[0]).then(() => cleanCache(cacheName, maxSize));
       }
     });
   });
